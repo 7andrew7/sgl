9ba99ad3e9e7acc671f86a850806dd9b186dcb17
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-11 09:17:19 -0700

    AddEdge method

--- src/graph.h
+++ src/graph.h
@@ -9,6 +9,12 @@ template <class NodeKey>
 class Graph {
   typedef std::unordered_set<NodeKey> NodeSet;
 
+  void AddEdge(const NodeKey &source, const NodeKey &dest) {
+    edges_[source] = dest;
+    nodes_.insert(source);
+    nodes_.insert(dest);
+  }
+
  private:
   NodeSet nodes_;
   std::unordered_map<NodeKey, NodeSet> edges_;
